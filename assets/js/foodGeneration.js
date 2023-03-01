6bfd393d258913bdc85ee6e6769587e9043dddc3
MSchuur <mschuur31@gmail.com>
2023-03-01 19:36:15 -0400

    Again

--- assets/js/foodGeneration.js
+++ assets/js/foodGeneration.js
@@ -1,16 +1,21 @@
-var key = '3fe254c2ed7141d18ff352826d6790e2';
+var key = '2929663d6dd7ee94db20d78f5c2a2235';
+var applicationId = '86631bdd';
 console.log(key)
-var apiUrl = 'https://api.edamam.com/api/recipes/v2/?app_id=86631bdd&app_key=2929663d6dd7ee94db20d78f5c2a2235&type=public';
+var apiUrl = 'https://api.edamam.com/api/recipes/v2/?app_id=86631bdd&app_key='+ key + '&type=public';
 
 fetch(apiUrl)
     .then(function (response) {
-        console.log(response);
-        // response.JSON();
-        
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data)
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
     })
+    .catch(function (error) {
+      alert('Unable to connect to GitHub');
+    });
 
-    // https://api.spoonacular.com/recipes/716429/information?apiKey=YOUR-API-KEY&includeNutrition=true
 
-    // API Edamam 2929663d6dd7ee94db20d78f5c2a2235
-
-    // Application ID 86631bdd
+   
