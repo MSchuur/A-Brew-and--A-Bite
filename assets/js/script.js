bc77a6d4367b7ff55bedfab77f9aaa3d4022744e
Amanda Lacroix <amanda_lacroix@hotmail.com>
2023-03-01 22:49:05 -0400

    add images of beer & food to images folder, revise script.js images card carousel & index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,26 @@
+
 // Array for the data that goes into the card carousel template
 var carouselImagesVar = [
     {
-        name: "Pale Ale",
+        image: "assets/Images/brad-qIAw1kNk1p8-unsplash.jpg",
+      
+    },
+
+    {
         image: "assets/Images/beerBottleForFoodPageHolder.jpg",
       
     },
 
     {
-        name: "Sour",
         image: "assets/Images/beerBottleForFoodPageHolder.jpg",
       
     },
 
     {
-        name: "Lager",
-        image: "assets/Images/beerBottleForFoodPageHolder.jpg",
-      
-    },
-
-    {
-        name: "Stout",
         image: "assets/Images/beerBottleForFoodPageHolder.jpg",
     },
 
     {
-        name: "IPA",
         image: "assets/Images/beerBottleForFoodPageHolder.jpg",
       
     },
@@ -41,9 +37,6 @@ var carouselImagesVar = [
         carousel += 
         `<section class="section">
             <div class="card" id="carousel">
-            <header class="card-header">
-                <p class="card-header-title is-centered is-size-3"> ${ imageCard.name} </p>
-            </header>
             <div class="card-image image is-96X96">
                 <img src=${ imageCard.image}>
             </div>
