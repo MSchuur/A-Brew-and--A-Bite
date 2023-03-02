f0bc86d21fecfb498ba25920efb9e0a3ee8990ac
APOLAKl <122183428+APOLAKl@users.noreply.github.com>
2023-03-02 19:59:46 -0400

    Revert "First commit - deleted multiple unecessary codes"
    This reverts commit c578eddd789953d20d024ece343213c64a9d76ab.

--- assets/js/beerGeneration.js
+++ assets/js/beerGeneration.js
@@ -15,7 +15,9 @@
 
 const beerBtn = $('#beerBtn');
 
+
 const randomBeer = $('.random-beer')
+const displayDescription = $('.description')
 
 function getBeer(event) {
   // // event.preventDefault();
@@ -23,78 +25,30 @@ function getBeer(event) {
     for(var i = 0; i < 3; i++) {
 
 
-    const beerApi = "https://api.punkapi.com/v2/beers/random";
-
-    fetch(beerApi)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-
-      const beerName = data[0].name;
-      const beerName1 = data[1].name;
-      console.log(beerName1)
-      const beerDescription = data[0].description;
-      const beerDescription1 = data[1].description;
-      // console.log(beerDescription)
-      const beerIngredients = data[0].ingredients.malt[0];
-      const beerIngredients1 = data[1].ingredients.malt[0];
-      // console.log(beerIngredients)
-      const volumeValue = data[0].volume.value;
-      const volumeValue1 = data[1].volume.value;
-      // console.log(volumeValue)
-      const volumeUnit = data[0].volume.unit;
-      const volumeUnit1 = data[1].volume.unit;
-      // console.log(volumeUnit)
-      // const beerImg = data[0].image_url;
-      // console.log(beerImg)
-      const beerTag = data[0].tagline;
-      const beerTag1 = data[1].tagline;
-      // console.log(beerTag)
-      const beerPair1 = data[0].food_pairing[0];
-      const beerPair2 = data[0].food_pairing[1];
-      const beerPair3 = data[0].food_pairing[2];
-      const beerPair4 = data[1].food_pairing[0];
-      const beerPair5 = data[1].food_pairing[1];
-      const beerPair6 = data[1].food_pairing[2];
-      // console.log(beerPair1)
-      // console.log(beerPair2)
-      // console.log(beerPair3)
-      
-      showRandomBeer1(beerName, beerDescription, beerIngredients, volumeValue, volumeUnit, beerTag, beerPair1, beerPair2, beerPair3)
-    })
+    const beerName = data[0].name;
+    console.log(beerName)
+    const beerDescription = data[0].description;
+    console.log(beerDescription)
+    const beerIngredients = data[0].ingredients;
+    console.log(beerIngredients)
+    const volumeValue = data[0].volume.value;
+    console.log(volumeValue)
+    const volumeUnit = data[0].volume.unit;
+    console.log(volumeUnit)
+    // const beerImg = data[0].image_url;
+    // console.log(beerImg)
+    const beerTag = data[0].tagline;
+    console.log(beerTag)
+    const beerPair1 = data[0].food_pairing[0];
+    const beerPair2 = data[0].food_pairing[1];
+    const beerPair3 = data[0].food_pairing[2];
+    console.log(beerPair1)
+    console.log(beerPair2)
+    console.log(beerPair3)
     
-    // fetch(beerApi)
-    // .then(res => res.json())
-    // .then(data => {
-    //   console.log(data);
-
-    //   const beerName = data[0].name;
-    //   console.log(beerName)
-    //   const beerDescription = data[0].description;
-    //   console.log(beerDescription)
-    //   const beerIngredients = data[0].ingredients.malt[0];
-    //   console.log(beerIngredients)
-    //   const volumeValue = data[0].volume.value;
-    //   console.log(volumeValue)
-    //   const volumeUnit = data[0].volume.unit;
-    //   console.log(volumeUnit)
-    //   // const beerImg = data[0].image_url;
-    //   // console.log(beerImg)
-    //   const beerTag = data[0].tagline;
-    //   console.log(beerTag)
-    //   const beerPair1 = data[0].food_pairing[0];
-    //   const beerPair2 = data[0].food_pairing[1];
-    //   const beerPair3 = data[0].food_pairing[2];
-    //   console.log(beerPair1)
-    //   console.log(beerPair2)
-    //   console.log(beerPair3)
-      
-    //   showRandomBeer2(beerName, beerDescription, beerIngredients, volumeValue, volumeUnit, beerTag, beerPair1, beerPair2, beerPair3)
-    // })
-
+    showRandomBeer1(beerName, beerDescription, beerIngredients, volumeValue, volumeUnit, beerTag, beerPair1, beerPair2, beerPair3)
+  })
   
-  }
-
 }
 
 // RANDOM BEER 1
@@ -129,36 +83,4 @@ function showRandomBeer1(beerName, beerDescription, beerIngredients, volumeValue
   $("#show-random-beer").append(cardDiv);
 }
 
-// function showRandomBeer2(beerName1, beerDescription1, beerIngredients1, volumeValue1, volumeUnit1, beerTag1, beerPair1, beerPair2, beerPair3) {
-//   $("#show-random-beer").empty()
-//   var card2Div = $("<div>");
-//   card2Div.addClass("card");
-//   card2Div.html(`
-//   <div class="card-body">
-//     <h3>
-//     <span>Beer: ${beerName1}</span>
-//     </h3>
-//     <h5>
-//       <span>${beerTag1}</span>
-//       <ul>
-//         <li>Ingredients: ${beerIngredients1} not yet done, coming soon!</li>
-//         <li>Volume: ${volumeValue1} ${volumeUnit1}</li>
-//       </ul>
-//       <span>Description: [${beerDescription1}]</span>
-//     </h5>
-//     <h4>Suggested Food Pairings</h4>
-//     <h5>
-//       <ul>
-//         <li>Pair1: ${beerPair4}</li>
-//         <li>Pair2: ${beerPair5}</li>
-//         <li>Pair3: ${beerPair6}</li>
-//       </ul>
-//     </h5>
-//   </div>
-//   `);
-
-//   $("#show-random-beer2").append(card2Div);
-// }
-
-beerBtn.on('click', getBeer,);
-
+beerBtn.on('click', getBeer);
