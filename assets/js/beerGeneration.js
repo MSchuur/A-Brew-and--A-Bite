c578eddd789953d20d024ece343213c64a9d76ab
APOLAKl <122183428+APOLAKl@users.noreply.github.com>
2023-03-02 19:39:33 -0400

    First commit - deleted multiple unecessary codes

--- assets/js/beerGeneration.js
+++ assets/js/beerGeneration.js
@@ -1,8 +1,6 @@
 const beerBtn = $('#beerBtn');
 
-
 const randomBeer = $('.random-beer')
-const displayDescription = $('.description')
 
 function getBeer(event) {
   // // event.preventDefault();
@@ -18,7 +16,7 @@ function getBeer(event) {
     console.log(beerName)
     const beerDescription = data[0].description;
     console.log(beerDescription)
-    const beerIngredients = data[0].ingredients;
+    const beerIngredients = data[0].ingredients.malt[0];
     console.log(beerIngredients)
     const volumeValue = data[0].volume.value;
     console.log(volumeValue)
@@ -37,6 +35,36 @@ function getBeer(event) {
     
     showRandomBeer1(beerName, beerDescription, beerIngredients, volumeValue, volumeUnit, beerTag, beerPair1, beerPair2, beerPair3)
   })
+  
+  fetch(beerApi)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+
+    const beerName = data[0].name;
+    console.log(beerName)
+    const beerDescription = data[0].description;
+    console.log(beerDescription)
+    const beerIngredients = data[0].ingredients.malt[0];
+    console.log(beerIngredients)
+    const volumeValue = data[0].volume.value;
+    console.log(volumeValue)
+    const volumeUnit = data[0].volume.unit;
+    console.log(volumeUnit)
+    // const beerImg = data[0].image_url;
+    // console.log(beerImg)
+    const beerTag = data[0].tagline;
+    console.log(beerTag)
+    const beerPair1 = data[0].food_pairing[0];
+    const beerPair2 = data[0].food_pairing[1];
+    const beerPair3 = data[0].food_pairing[2];
+    console.log(beerPair1)
+    console.log(beerPair2)
+    console.log(beerPair3)
+    
+    showRandomBeer2(beerName, beerDescription, beerIngredients, volumeValue, volumeUnit, beerTag, beerPair1, beerPair2, beerPair3)
+  })
+
   
 }
 
@@ -72,4 +100,36 @@ function showRandomBeer1(beerName, beerDescription, beerIngredients, volumeValue
   $("#show-random-beer").append(cardDiv);
 }
 
-beerBtn.on('click', getBeer);
+function showRandomBeer2(beerName, beerDescription, beerIngredients, volumeValue, volumeUnit, beerTag, beerPair1, beerPair2, beerPair3) {
+  $("#show-random-beer").empty()
+  var card2Div = $("<div>");
+  card2Div.addClass("card");
+  card2Div.html(`
+  <div class="card-body">
+    <h3>
+    <span>Beer: ${beerName}</span>
+    </h3>
+    <h5>
+      <span>${beerTag}</span>
+      <ul>
+        <li>Ingredients: ${beerIngredients} not yet done, coming soon!</li>
+        <li>Volume: ${volumeValue} ${volumeUnit}</li>
+      </ul>
+      <span>Description: [${beerDescription}]</span>
+    </h5>
+    <h4>Suggested Food Pairings</h4>
+    <h5>
+      <ul>
+        <li>Pair1: ${beerPair1}</li>
+        <li>Pair2: ${beerPair2}</li>
+        <li>Pair3: ${beerPair3}</li>
+      </ul>
+    </h5>
+  </div>
+  `);
+
+  $("#show-random-beer2").append(card2Div);
+}
+
+beerBtn.on('click', getBeer,);
+
