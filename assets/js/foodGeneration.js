bfecbd57727138eabe866ad812d7ed94e84f69f2
MSchuur <mschuur31@gmail.com>
2023-03-06 03:41:44 -0400

    Finished the food genertation with local storage.

--- assets/js/foodGeneration.js
+++ assets/js/foodGeneration.js
@@ -2,62 +2,88 @@
 
 var key = '2929663d6dd7ee94db20d78f5c2a2235';
 var applicationId = '86631bdd';
-
-
-var pairingListEl = document.querySelector('#storedPairing')
-var pairings = ['Jamaican jerk chicken wings', 'Corn dogs', 'Strawberry ice cream sundae']
-var apiUrlName = '';
-var choiceEl = document.querySelector('.choiceMenu')
+var pairingListEl = document.querySelector('#storedPairing');
+var genBeerNameEl = document.querySelector('#chosenBeerName');
+var choiceEl = document.querySelector('.choiceMenu');
+var storedNamesListEl = document.querySelector('#storedNamesList');
+var recipeChoiceEl = document.querySelector('#choicesCard');
 var goBackBtnEl = document.querySelector('#foodGoBack');
+var apiUrlName = '';
 
+// Creating Gobal variables to get the local storage from the beer generation page
+
+var newBeerName = '';
+var newBeerPairing1;
+var newBeerPairing2;
+var newBeerPairing3;
 
 // Render the food pairing suggestions from the beer generation page that were stored in local storage
 
 function renderPairing() {
     pairingListEl.innerHTML = '';
-    var pairings = JSON.parse(localStorage.getItem('pairings')) || [];
-    for(var i= pairings.length-1; i >=0; i--) {
-        var pairingEl = document.createElement('button');
-        pairingEl.classList.add('button', 'my-3' ,'is-link', 'is-rounded', 'is-hover', 'is-fullwidth', 'is-focused')
-        pairingEl.innerText = pairings[i];
-        pairingListEl.appendChild(pairingEl);
+    var beerGenData = JSON.parse(localStorage.getItem('personalStorage')) || [];
+
+    // Create the last searched beers list with buttons
+    for (var i = beerGenData.length-1; i >= 0; i--) {
+      var storedNameEl = document.createElement('button');
+      storedNameEl.classList.add('button', 'my-3' ,'is-link', 'is-rounded', 'is-hover', 'is-fullwidth', 'is-focused');
+      storedNameEl.innerText = beerGenData[i].beerName;
+      storedNameEl.setAttribute('value', i);
+      storedNamesListEl.appendChild(storedNameEl);
     }
-}
+    
+    // Getting the last saved item and subtracting 1 to get the array index
+    var arrayLength = beerGenData.length-1;
+    
+    // newBeerImg = beerGenData[arrayLength].beerImg;
+    newBeerName = beerGenData[arrayLength].beerName;
+    newBeerPairing1 = beerGenData[arrayLength].beerPairing1;
+    newBeerPairing2 = beerGenData[arrayLength].beerPairing2;
+    newBeerPairing3 = beerGenData[arrayLength].beerPairing3;
 
-// Store the chosen recipe into local storage to be used on the recipe generation page
+    // Adding the generated beer name to the text as an H3
+    genBeerNameEl.textContent = newBeerName;
+    
+    // Creating the buttons for the 3 suggested food pairing form storage    
+    var pairingEl1 = document.createElement('button');
+    var pairingEl2 = document.createElement('button');
+    var pairingEl3 = document.createElement('button');
+    
+    pairingEl1.classList.add('button', 'my-3' ,'is-link', 'is-rounded', 'is-hover', 'is-fullwidth', 'is-focused');
+    pairingEl2.classList.add('button', 'my-3' ,'is-link', 'is-rounded', 'is-hover', 'is-fullwidth', 'is-focused');
+    pairingEl3.classList.add('button', 'my-3' ,'is-link', 'is-rounded', 'is-hover', 'is-fullwidth', 'is-focused');
 
-function storeRecipeName(recipeLabel) {
-  var recipeLabels = JSON.parse(localStorage.getItem('recipe')) || [];
-   
-  for (var i = 0; i < recipeLabels.length; i ++ ) {
-      if (recipeLabels[i] === recipeLabel) {
-          return
-      }
-  }
-  if (recipeLabels.length >= 5){
-      recipeLabels.shift();
-  }
-  recipeLabels.push(recipeLabel);
-  localStorage.setItem('recipe', JSON.stringify(recipeLabels));
-}
+    pairingEl1.setAttribute('id', 'pairing1');
+    pairingEl2.setAttribute('id', 'pairing2');
+    pairingEl3.setAttribute('id', 'pairing3');
 
+    pairingEl1.innerText = newBeerPairing1;
+    pairingEl2.innerText = newBeerPairing2;
+    pairingEl3.innerText = newBeerPairing3;
+
+    pairingListEl.appendChild(pairingEl1);
+    pairingListEl.appendChild(pairingEl2);
+    pairingListEl.appendChild(pairingEl3);
+ }
+
+// Splitting the generated food pairing name so it can be used in the Edamam API
 function getRecipeSuggestions(choice) {
-   
-    var recipeName = choice;
-    var recipeArray = recipeName.split(' ');
-    for (i=0; i < recipeArray.length; i++) {
-        apiUrlName = apiUrlName + recipeArray[i]+ ',+';
-        
-    }
-    var apiUrl = 'https://api.edamam.com/api/recipes/v2/?app_id=86631bdd&app_key='+ key + '&q=' + apiUrlName + '&type=public';
     
+  var recipeName = ' ';
+  var recipeName = choice;
+  var recipeArray = recipeName.split(' ');
+  for (i=0; i < recipeArray.length; i++) {
+    apiUrlName = '';
+    apiUrlName = apiUrlName + recipeArray[i]+ ',+';
+  }
+  var apiUrl = 'https://api.edamam.com/api/recipes/v2/?app_id=86631bdd&app_key='+ key + '&q=' + apiUrlName + '&type=public';
     
+    // Fetching the data required in the food API
     fetch(apiUrl)
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
-        console.log(data)
-          displayRecipe(data);
+        displayRecipe(data);
         });
       } else {
         alert('Error: ' + response.statusText);
@@ -68,11 +94,10 @@ function getRecipeSuggestions(choice) {
     });
 };
 
+// Creating the cards and redering the data from the API
 function displayRecipe(data) {
     
   for (var i = 0; i < 3; i++) {
-    
-  var recipeChoiceEl = document.querySelector('#choicesCard');
     var newCard = document.createElement('div');
     newCard.classList = 'card column is-one-third newCard';
     recipeChoiceEl.appendChild(newCard);
@@ -98,16 +123,40 @@ function displayRecipe(data) {
   }
 }
 
-renderPairing(pairings);
+// Initialize the the page and call the render of the pairing buttons
+renderPairing();
 
+// Creates the pairing cards
 pairingListEl.addEventListener('click', function(event) {
   var element = event.target;
   element = element.innerHTML;
+  var clear = document.getElementById(choicesCard);
+  recipeChoiceEl.innerHTML = ' ';
   getRecipeSuggestions(element);
   });
 
+// Opens a new window/tab url with the recipe 
 choiceEl.addEventListener('click', function(event){
   var recipeClick = event.target;
-  console.log(recipeClick.value);
   window.open(recipeClick.value);
 });
+
+// Renders the pairing list for a save beer
+storedNamesListEl.addEventListener('click', function(event) {
+  
+  var storedName = event.target;
+  storedName = storedName.innerHTML;
+  recipeChoiceEl.innerHTML = '';
+  var beerGenData = JSON.parse(localStorage.getItem('personalStorage')) || [];
+  for (var i = 0; i < beerGenData.length; i++) {
+    if (beerGenData[i].beerName === storedName) {
+      newBeerPairing1 = beerGenData[i].beerPairing1;
+      newBeerPairing2 = beerGenData[i].beerPairing2;
+      newBeerPairing3 = beerGenData[i].beerPairing3;
+
+      document.getElementById('pairing1').innerText = newBeerPairing1;
+      document.getElementById('pairing2').innerText = newBeerPairing2;
+      document.getElementById('pairing3').innerText = newBeerPairing3;    
+    }
+  }
+})
